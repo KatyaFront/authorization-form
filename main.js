a26b61a38c8d9d51781a5f05b05a6914c5b6cec8
Ekaterina Sai <ktrnsajbel@gmail.com>
2024-08-06 13:51:43 +0500

    change toggle show password

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const form = document.getElementById('form');
 const toggleShowPassword = document.getElementById('show-password');
-const imgEye = document.getElementById('img-eye');
-const password = document.getElementById('password');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 });
 
-toggleShowPassword.addEventListener('click', () => {
+toggleShowPassword.addEventListener('click', (event) => {
+  const spanElement = event.target.closest('.form__span');
+  const password = spanElement
+    .closest('.form__input-label')
+    .querySelector('.form__input');
   const type =
     password.getAttribute('type') === 'password' ? 'text' : 'password';
   password.setAttribute('type', type);
+
+  const imgEye = event.target;
   imgEye.classList.toggle('open-close-eye');
 });
